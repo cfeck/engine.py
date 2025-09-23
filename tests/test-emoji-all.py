c04532b3e5f55ea75df554b52d0a1145dd369e01
Christoph Feck <cfeck@kde.org>
2025-09-23 13:39:41 +0200

    Remove offset from Emoji position and port to Grid loop
    The offset was required on the old code which used Text class.

--- tests/test-emoji-all.py
+++ tests/test-emoji-all.py
@@ -3,12 +3,11 @@ from engine import *
 
 app = App()
 
-for y in range(48):
-    for x in range(64):
-        text = Emoji(0x1F000 + x + 64 * y, 10)
-        text.x = 10 * x + 5
-        text.y = 10 * y + 5
-        app.show(text)
+for x, y in Grid(64, 48):
+    text = Emoji(0x1F000 + x + 64 * y, 10)
+    text.x = 10 * x
+    text.y = 10 * y
+    app.show(text)
 
 app.exec()
 
